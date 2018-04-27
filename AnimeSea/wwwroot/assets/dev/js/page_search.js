c0f4b36ad89af7332ae3e7279dcde2778f8f2dc7
Gerard Smit <contact@gerard.pw>
2018-04-27 06:47:40 -0700

    Add search query to the URL (#2)
    * Added the query to the URL
    
    * Remove search hash if the query is empty
    
    * Removed unnecessarily return

--- AnimeSea/wwwroot/assets/dev/js/page_search.js
+++ AnimeSea/wwwroot/assets/dev/js/page_search.js
@@ -8,23 +8,57 @@ const app = new Vue({
         results: [],
         resultsEmpty: false,
         next: null,
-        searching: false
+        searching: false,
+        autoSearch: true
+    },
+    created: function () {
+        this.onHashChange();
+    },
+    mounted: function() {
+        window.addEventListener('hashchange', this.onHashChange, false);
+    },
+    beforeDestroy: function() {
+        window.removeEventListener('hashchange', this.onHashChange, false);
     },
     watch: {
         query: function() {
-            if (searchTimerId) {
-                clearTimeout(searchTimerId);
+            // Update hash
+            if (this.query) {
+                const hash = '#q=' + this.query;
+                if (history.pushState) {
+                    history.replaceState(null, null, hash);
+                } else {
+                    location.replace(hash);
+                }
+            } else {
+                if (history.pushState) {
+                    history.replaceState(null, null, window.location.pathname)
+                } else {
+                    location.replace('#');
+                }
             }
 
-            this.results = [];
-            this.resultsEmpty = false;
-
-            searchTimerId = setTimeout(() => {
-                this.search();
-            }, 800);
+            // Auto-search
+            if (this.autoSearch) {
+                if (searchTimerId) {
+                    clearTimeout(searchTimerId);
+                }
+    
+                this.results = [];
+                this.resultsEmpty = false;
+    
+                searchTimerId = setTimeout(() => {
+                    this.search();
+                }, 800);
+            }
         }
     },
     methods: {
+        onHashChange: function() {
+            if (location.hash.substr(0, 3) === '#q=') {
+                this.search(location.hash.substr(3));
+            }
+        },
         getCommaSeperated: function (listItems) {
             if (typeof listItems === "object") {
                 listItems = Object.values(listItems);
@@ -48,8 +82,23 @@ const app = new Vue({
 
             return response;
         },
-        search: function () {
-            if (this.query.length === 0 || this.searching) {
+        search: function (query) {
+            if (query) {
+                this.query = query;
+
+                // We disable auto-search because the 'query' is being watched.
+                // When the view was rendered, we re-enable auto-search.
+                if (this.autoSearch) {
+                    this.autoSearch = false;
+                    this.$nextTick(function () {
+                        this.autoSearch = true;
+                    })
+                }
+            } else {
+                query = this.query;
+            }
+
+            if (query.length === 0 || this.searching) {
                 return;
             }
 
@@ -64,7 +113,7 @@ const app = new Vue({
             this.resultsEmpty = false;
             this.next = null;
 
-            fetch(searchUrl + "?q=" + encodeURIComponent(this.query))
+            fetch(searchUrl + "?q=" + encodeURIComponent(query))
                 .then(res => res.json())
                 .then(res => {
                     this.searching = false;
